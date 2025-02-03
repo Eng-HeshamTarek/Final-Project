3e9502de47797ad060200bb02d3c0678f536fc1d
Eng-HeshamTarek <heshamtarek200271@gmail.com>
2025-02-03 02:12:15 +0200

    Add files via upload

---
+++ app.c
@@ -0,0 +1,26 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 7, 2024
+ *      Author: Medhat
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#include "../HAL/DC_MOTOR/DCMOTOR_interface.h"
+#include "../HAL/IMU/IMU_interface.h"
+
+
+int main()
+{
+
+	while(1)
+	{
+
+	}
+
+
+
+	return 0;
+}
